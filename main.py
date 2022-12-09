eaee9ab976405ea3e081843deccf7712e7f43886
Stasvdovin <vdovin.kontent@gmail.com>
2022-12-09 18:48:37 +0500

    main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from transformers import pipeline 
+from pydantic import BaseModel
+# Создаем класс для возможности передачи текста 
+class Item(BaseModel):
+    text: str
+app = FastAPI()
+# Создаем модель 
+classifier = pipeline(model="SkolkovoInstitute/russian_toxicity_classifier")
+@app.get('/')
+def root():
+    return {'message': 'Hello world'}
+# Функция для определения токсичных комментариве и передачи теска 
+@app.post("/predict/")
+def predict(item: Item):
+    """Анализ токсичных комменатиев"""
+
+    return classifier(item.text )[0]
